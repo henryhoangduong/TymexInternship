a59b697adb9511f93bcd0fdd985fee70cf9789d8
henryhoangduong <henryhoangduong@gmail.com>
2024-12-15 21:16:33 +0700

    commit

--- Challenge2/2.2.py
+++ Challenge2/2.2.py
@@ -12,15 +12,12 @@ def calculate_total_inventory_value(products):
     return total_value
 
 
+
 def find_most_expensive_product(products):
     if not products:
         return None
     most_expensive = max(products, key=lambda product: product.price)
     return most_expensive.name
-
-
-most_expensive_product = find_most_expensive_product(inventory)
-print(f"Most Expensive Product: {most_expensive_product}")
 
 
 def is_product_in_stock(products, product_name):
@@ -30,33 +27,34 @@ def is_product_in_stock(products, product_name):
     return False
 
 
-is_headphones_in_stock = is_product_in_stock(inventory, "Headphones")
-print(f"Headphones in Stock: {is_headphones_in_stock}")
-
-
 def sort_products(products, key, descending=True):
-    return sorted(
-        products, key=lambda product: getattr(product, key), reverse=descending
-    )
+    return sorted(products, key=lambda product: getattr(product, key), reverse=descending)
 
 
-sorted_by_price = sort_products(inventory, "price", descending=True)
-print("Products sorted by price (descending):")
-for product in sorted_by_price:
-    print(f"{product.name}: ${product.price}, Quantity: {product.quantity}")
-
-sorted_by_quantity = sort_products(inventory, "quantity", descending=False)
-print("\nProducts sorted by quantity (ascending):")
-for product in sorted_by_quantity:
-    print(f"{product.name}: ${product.price}, Quantity: {product.quantity}")
-
-if __name__ == "__main__":
+if __name__=="__main__":
     inventory = [
-        Product("Laptop", 999.99, 5),
-        Product("Smartphone", 499.99, 10),
-        Product("Tablet", 299.99, 0),
-        Product("Smartwatch", 199.99, 3),
+    Product("Laptop", 999.99, 5),
+    Product("Smartphone", 499.99, 10),
+    Product("Tablet", 299.99, 0),
+    Product("Smartwatch", 199.99, 3)
     ]
-
+    
     total_value = calculate_total_inventory_value(inventory)
-print(f"Total Inventory Value: ${total_value:.2f}")
+    print(f"Total Inventory Value: ${total_value:.2f}") 
+
+    most_expensive_product = find_most_expensive_product(inventory)
+    print(f"Most Expensive Product: {most_expensive_product}") 
+
+    is_headphones_in_stock = is_product_in_stock(inventory, "Headphones")
+    print(f"Headphones in Stock: {is_headphones_in_stock}")  
+
+    sorted_by_price = sort_products(inventory, "price", descending=True)
+    print("Products sorted by price (descending):")
+    
+    for product in sorted_by_price:
+        print(f"{product.name}: ${product.price}, Quantity: {product.quantity}")
+
+    sorted_by_quantity = sort_products(inventory, "quantity", descending=False)
+    print("\nProducts sorted by quantity (ascending):")
+    for product in sorted_by_quantity:
+        print(f"{product.name}: ${product.price}, Quantity: {product.quantity}")
